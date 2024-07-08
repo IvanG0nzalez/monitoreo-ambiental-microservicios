eacb50083d632243b231066ae3f8b05ddce79beb
Davud404 <david.a.campoverde@unl.edu.ec>
2024-07-07 23:14:19 -0500

    Sensores y registros

---
+++ microservicio-sensores/app/controls/RegistroControl.js
@@ -0,0 +1,95 @@
+"use strict";
+var models = require('../models');
+var registros = models.registro_climatico;
+var sensor = models.sensor;
+var sequelize = models.sequelize;
+
+class RegistroControl{
+    async listar_hoy(req,res){
+        const fechaActual = new Date().toISOString().slice(0,10);
+        var lista = await registros.findAll({
+            where:{fecha:fechaActual},
+            include:[{
+                model: models.sensor, as: "sensor",
+                attributes:['alias', 'ip', 'tipo_medicion','external_id'],
+            },],
+            attributes:['fecha','hora','valor_medido','external_id'],
+        });
+        if(lista.length === 0){
+            res.status(200);
+            res.json({msg:"OK", tag:"No existen registros el día de hoy", datos:lista});
+        }else{
+            res.status(200);
+            res.json({msg:"OK", code:200, datos:lista});
+        }
+    }
+
+    async listar(req,res){
+        var lista = await registros.findAll({
+            include:[{
+                model:models.sensor, as:"sensor",
+                attributes:['alias','ip','tipo_medicion','external_id'],
+            },],
+            attributes:['fecha','hora','valor_medido','external_id'],
+        });
+        res.status(200);
+        res.json({msg:"OK",code:200,datos:lista});
+    }
+
+    async listar_por_fecha(req,res){
+        const fecha = req.params.fecha;
+        var lista = await registros.findAll({
+            where:{fecha:fecha},
+            include:[{
+                model:models.sensor, as:"sensor",
+                attributes:['alias','ip','tipo_medicion','external_id'],
+            },],
+            attributes:['fecha','hora','valor_medido','external_id'],
+        });
+        if(lista.length === 0){
+            res.status(200);
+            res.json({msg:"OK", tag:"No existen registros de la fecha "+fecha, datos:lista});
+        }else{
+            res.status(200);
+            res.json({msg:"OK", code:200, datos:lista});
+        }
+    }
+
+    async guardar_manual(req,res){
+        if(req.body.hasOwnProperty('valor_medido') &&
+            req.body.hasOwnProperty('sensor')){
+            var uuid = require("uuid");
+            try {
+                var sensorAux = await sensor.findOne({where:{external_id:req.body.sensor}});
+                var fecha_hora_actual = new Date();
+                var fecha_actual = fecha_hora_actual.toISOString().slice(0,10);
+                //var hora_actual = fecha_hora_actual.getHours()+":"+fecha_hora_actual.getMinutes()+":"+fecha_hora_actual.getSeconds();
+                var hora_actual = fecha_hora_actual.getUTCHours() + ":" + fecha_hora_actual.getUTCMinutes() + ":" + fecha_hora_actual.getUTCSeconds();
+                var data={
+                    fecha:fecha_actual,
+                    hora:hora_actual,
+                    valor_medido: req.body.valor_medido,
+                    id_sensor: sensorAux.id,
+                    external_id:uuid.v4(),
+                }
+                var result = await registros.create(data);
+                if(result === null){
+                    res.status(401);
+                    res.json({ msg: "Error", tag: "No se guardó el registro climático", code: 401 });
+                }else{
+                    res.status(200);
+                    res.json({msg:"OK", tag:"Registro climático guardado", code:200});
+                }
+            } catch (error) {
+                res.status(404);
+                res.json({msg:"Error",tag:"El sensor no existe",code:404});
+            }
+        }else{
+            res.status(400);
+            res.json({msg:"Error", tag:"Faltan datos",code:400});
+        }
+    }
+
+    //TODO xd Listar últimos registros
+}
+module.exports = RegistroControl;
