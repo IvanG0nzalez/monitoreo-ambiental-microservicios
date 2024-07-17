0b1731b0a5bbbf010b836b2ae56c723091514974
Davud404 <david.a.campoverde@unl.edu.ec>
2024-07-16 23:22:34 -0500

    listar usuarios

--- microservicio-sensores/routes/api.js
+++ microservicio-sensores/routes/api.js
@@ -4,15 +4,16 @@ const sensorC = require('../app/controls/SensorControl');
 let sensorControl = new sensorC();
 const registrosC = require('../app/controls/RegistroControl');
 let registrosControl = new registrosC();
+const auth = require('../middlewares/authMiddleware');
 
 //TODO agregar auth y jsonwebtoken
 //API sensores
 router.get('/sensores',sensorControl.listar);
-router.get('/sensores/buscar/:external', sensorControl.obtener_sensor);
-router.post('/sensores/guardar', sensorControl.guardar);
-router.get('/sensores/registros/buscar/:external', sensorControl.obtener_registros_climaticos);
-router.patch('/sensores/modificar/:external',sensorControl.modificar);
-router.get('/sensores/ultimo_registro', sensorControl.ultimo_registro);
+router.get('/sensores/buscar/:external', auth, sensorControl.obtener_sensor);
+router.post('/sensores/guardar', auth, sensorControl.guardar);
+router.get('/sensores/registros/buscar/:external', auth, sensorControl.obtener_registros_climaticos);
+router.patch('/sensores/modificar/:external',auth,sensorControl.modificar);
+router.get('/sensores/ultimo_registro',auth, sensorControl.ultimo_registro);
 
 //API registros
 router.get('/registros/listar/hoy', registrosControl.listar_hoy);
