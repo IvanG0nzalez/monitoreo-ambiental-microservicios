1ef27b9804c997ab4ad5d3888f7279553186182b
Davud404 <david.a.campoverde@unl.edu.ec>
2024-07-18 21:14:16 -0500

    arreglo crear usuarios

--- microservicio-usuarios/app/controllers/UsuarioController.js
+++ microservicio-usuarios/app/controllers/UsuarioController.js
@@ -82,22 +82,16 @@ class UsuarioController {
 
     async crear(req, res) {
         const { correo, nombre_usuario, clave, cedula, nombres, apellidos, external_rol } = req.body;
-
         if (!correo || !nombre_usuario || !clave || !external_rol || !cedula) {
             return res.status(400).json({ msg: 'Parámetros incorrectos', code: 400, datos: {} });
         }
-
         const rolAux = await rol.findOne({ where: { external_id: external_rol } });
-
         if (!rolAux) {
             return res.status(404).json({ msg: 'Rol no encontrado', code: 404, datos: {} });
         }
-
         const transaction = await models.sequelize.transaction();
-
         try {
             const UUID = require('uuid');
-
             const nuevo_usuario = await usuario.create({
                 cedula: cedula,
                 nombres: nombres,
@@ -105,32 +99,26 @@ class UsuarioController {
                 id_rol: rolAux.id,
                 external_id: UUID.v4(),
             }, { transaction });
-
             if (!nuevo_usuario) {
                 await transaction.rollback();
                 return res.status(500).json({ msg: 'Error al crear usuario', code: 500, datos: {} });
             }
-
             await sendMessage('usuario_creado', {
                 correo,
                 nombre_usuario,
                 clave,
                 id_usuario: nuevo_usuario.id
             });
-
             await consumeMessage('cuenta_creada', async (message) => {
                 const { success, msg } = message;
 
-                if (success) {
-                    await transaction.commit();
-                    return res.status(201).json({ msg: 'Usuario y cuenta creados', code: 201 });
-                } else {
+                if (!success) {
                     await transaction.rollback();
                     return res.status(500).json({ msg: 'Error al crear usuario', code: 500, datos: {} });
                 }
             });
-
-
+            await transaction.commit();
+            return res.status(201).json({ msg: 'Usuario y cuenta creados', code: 201 });
         } catch (error) {
             await transaction.rollback();
             return res.status(500).json({ msg: 'Error al crear usuario', code: 500, datos: error });
