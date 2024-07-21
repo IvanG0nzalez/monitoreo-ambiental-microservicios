e8c3f46da79dc8321750bb2f095e7084016a2de6
IvanG0nzalez <ivanalegonzalez3@gmail.com>
2024-07-21 00:13:30 -0500

    Refactor API routes for cuentas endpoints

--- microservicio-cuentas/routes/api.js
+++ microservicio-cuentas/routes/api.js
@@ -8,9 +8,6 @@ const auth = require('../middlewares/authMiddleware');
 
 router.post('/cuentas/inicio_sesion', cuentaControl.inicio_sesion);
 router.get('/cuentas', auth, cuentaControl.listar);
-router.post('/cuentas/crear', cuentaControl.crear);
 router.get('/cuentas/:id_usuario', auth, cuentaControl.obtener);
-router.patch('/cuentas/:id_usuario', auth, cuentaControl.actualizar);
-router.delete('/cuentas/:id_usuario', auth, cuentaControl.eliminar);
 
 module.exports = router;
