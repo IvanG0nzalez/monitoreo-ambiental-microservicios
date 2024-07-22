c2021a1ee2546dbd2080537bb50b68636e9a11e4
IvanG0nzalez <ivanalegonzalez3@gmail.com>
2024-07-21 22:30:37 -0500

    Add login validation schema and API endpoints for cuentas
    - Added login validation schema using Yup
    - Created API endpoints for cuentas, including inicio_sesion, listar, and obtener

--- frontend-microservicios/src/app/authentication/auth/AuthLogin.tsx
+++ frontend-microservicios/src/app/authentication/auth/AuthLogin.tsx
@@ -3,12 +3,16 @@ import {
   Box,
   Typography,
   FormGroup,
-  FormControlLabel,
   Button,
   Stack,
-  Checkbox,
+  TextField,
 } from "@mui/material";
 import Link from "next/link";
+import { useForm, Controller } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { loginSchema } from "@/hooks/ValidationSchemas";
+import { api_cuentas } from "@/hooks/Api";
+import { useSnackbar } from "notistack";
 
 import CustomTextField from "@/app/(DashboardLayout)/components/forms/theme-elements/CustomTextField";
 
@@ -18,81 +22,116 @@ interface loginType {
   subtext?: JSX.Element | JSX.Element[];
 }
 
-const AuthLogin = ({ title, subtitle, subtext }: loginType) => (
-  <>
-    {title ? (
-      <Typography fontWeight="700" variant="h2" mb={1}>
-        {title}
-      </Typography>
-    ) : null}
+const AuthLogin = ({ title, subtitle, subtext }: loginType) => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(loginSchema),
+  });
 
-    {subtext}
+  const { enqueueSnackbar } = useSnackbar();
 
-    <Stack>
-      <Box>
-        <Typography
-          variant="subtitle1"
-          fontWeight={600}
-          component="label"
-          htmlFor="username"
-          mb="5px"
-        >
-          Correo Electronico
+  const onSubmit = async (data: any) => {
+    try {
+      const response = await api_cuentas.inicio_sesion(data);
+      console.log(response);
+      
+      if (response.status === 200) {
+        enqueueSnackbar("Inicio de sesión exitoso", { variant: "success" });
+      } else {
+        enqueueSnackbar("Error al iniciar sesión", { variant: "error" });
+      }
+    } catch (error) {
+      enqueueSnackbar("Error al iniciar sesión", { variant: "error" });
+    }
+  };
+
+  return (
+    <>
+      {title ? (
+        <Typography fontWeight="700" variant="h2" mb={1}>
+          {title}
         </Typography>
-        <CustomTextField variant="outlined" fullWidth />
-      </Box>
-      <Box mt="25px">
-        <Typography
-          variant="subtitle1"
-          fontWeight={600}
-          component="label"
-          htmlFor="password"
-          mb="5px"
-        >
-          Contraseña
-        </Typography>
-        <CustomTextField type="password" variant="outlined" fullWidth />
-      </Box>
-      <Stack
-        justifyContent="space-between"
-        direction="row"
-        alignItems="center"
-        my={2}
-      >
-        <FormGroup>
-          <FormControlLabel
-            control={<Checkbox defaultChecked />}
-            label="Recordar en este dispositivo"
-          />
-        </FormGroup>
-        <Typography
-          component={Link}
-          href="/"
-          fontWeight="500"
-          sx={{
-            textDecoration: "none",
-            color: "primary.main",
-          }}
-        >
-          ¿Olvido la constraseña ?
-        </Typography>
-      </Stack>
-    </Stack>
-    <Box>
-      <Button
-        color="primary"
-        variant="contained"
-        size="large"
-        fullWidth
-        component={Link}
-        href="/inicio"
-        type="submit"
-      >
-        Iniciar Sesión
-      </Button>
-    </Box>
-    {subtitle}
-  </>
-);
+      ) : null}
+
+      {subtext}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Stack>
+          <Box>
+            <Typography
+              variant="subtitle1"
+              fontWeight={600}
+              component="label"
+              htmlFor="correo"
+              mb="5px"
+            >
+              Correo Electronico
+            </Typography>
+            <Controller
+              name="correo"
+              control={control}
+              defaultValue=""
+              render={({ field }) => (
+                <CustomTextField
+                  {...field}
+                  type="text"
+                  variant="outlined"
+                  fullWidth
+                  error={errors.correo ? true : false}
+                  helperText={errors.correo ? errors.correo.message : ""}
+                />
+              )}
+            />
+          </Box>
+          <Box mt="25px">
+            <Typography
+              variant="subtitle1"
+              fontWeight={600}
+              component="label"
+              htmlFor="clave"
+              mb="5px"
+            >
+              Contraseña
+            </Typography>
+            <Controller
+              name="clave"
+              control={control}
+              defaultValue=""
+              render={({ field }) => (
+                <CustomTextField
+                  {...field}
+                  type="password"
+                  variant="outlined"
+                  fullWidth
+                  error={errors.clave ? true : false}
+                  helperText={errors.clave ? errors.clave.message : ""}
+                />
+              )}
+            />
+          </Box>
+          <Stack
+            justifyContent="space-between"
+            direction="row"
+            alignItems="center"
+            my={2}
+          ></Stack>
+        </Stack>
+        <Box>
+          <Button
+            color="primary"
+            variant="contained"
+            size="large"
+            fullWidth
+            type="submit"
+          >
+            Iniciar Sesión
+          </Button>
+        </Box>
+      </form>
+    </>
+  );
+};
 
 export default AuthLogin;
--- frontend-microservicios/src/app/authentication/login/page.tsx
+++ frontend-microservicios/src/app/authentication/login/page.tsx
@@ -8,7 +8,7 @@ import AuthLogin from "../auth/AuthLogin";
 
 const Login2 = () => {
   return (
-    <PageContainer title="Login" description="this is Login page">
+    <PageContainer title="Inicio de Sesión" description="Página de Inicio de Sesión">
       <Box
         sx={{
           position: "relative",
@@ -57,33 +57,6 @@ const Login2 = () => {
                   >
                     Tu aplicación de monitoreo favorita
                   </Typography>
-                }
-                subtitle={
-                  <Stack
-                    direction="row"
-                    spacing={1}
-                    justifyContent="center"
-                    mt={3}
-                  >
-                    <Typography
-                      color="textSecondary"
-                      variant="h6"
-                      fontWeight="500"
-                    >
-                      ¿Nuevo por aquí?
-                    </Typography>
-                    <Typography
-                      component={Link}
-                      href="/authentication/register"
-                      fontWeight="500"
-                      sx={{
-                        textDecoration: "none",
-                        color: "primary.main",
-                      }}
-                    >
-                      Crear una cuenta
-                    </Typography>
-                  </Stack>
                 }
               />
             </Card>
