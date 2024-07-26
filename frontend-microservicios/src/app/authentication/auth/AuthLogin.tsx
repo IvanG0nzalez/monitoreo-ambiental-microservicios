a1a7b1cfcceac8f73a5b1aff4738a69b441b63bf
Iván González <ivan.gonzalez@unl.edu.ec>
2024-07-26 11:18:38 -0500

    Update API endpoints for usuarios and roles to follow RESTful conventions, modify routes in api.js, and improve error handling in RolController.js

--- frontend-microservicios/src/app/authentication/auth/AuthLogin.tsx
+++ frontend-microservicios/src/app/authentication/auth/AuthLogin.tsx
@@ -38,7 +38,6 @@ const AuthLogin = ({ title, subtitle, subtext }: loginType) => {
   const onSubmit = async (data: any) => {
     try {
       const response = await api_cuentas.inicio_sesion(data);
-      console.log(response);
       
       if (response.data.code === 200) {
         enqueueSnackbar("Inicio de sesión exitoso", { variant: "success" });
