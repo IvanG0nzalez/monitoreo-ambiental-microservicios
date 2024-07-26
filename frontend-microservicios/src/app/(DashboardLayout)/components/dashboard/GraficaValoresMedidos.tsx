081a6a03631b34eeebbc56a81e109e4ac1f086ef
Iván González <ivan.gonzalez@unl.edu.ec>
2024-07-26 18:04:01 -0500

    Refactor RegistroControl to include tipo_medicion in the response

---
+++ frontend-microservicios/src/app/(DashboardLayout)/components/dashboard/GraficaValoresMedidos.tsx
@@ -0,0 +1,209 @@
+import React, { useEffect, useState } from 'react';
+import { Select, MenuItem, easing } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
+import DashboardCard from '@/app/(DashboardLayout)/components/shared/DashboardCard';
+import dynamic from "next/dynamic";
+import { api_registros } from "@/hooks/Api";
+import { useSnackbar } from "notistack";
+import { getToken } from '@/hooks/SessionUtils';
+const Chart = dynamic(() => import("react-apexcharts"), { ssr: false });
+
+const GraficaValoresMedidos = () => {
+    const token = getToken();
+    const { enqueueSnackbar } = useSnackbar();
+    const [registros, setRegistros] = useState([]);
+    const [registrosCO2, setRegistrosCO2] = useState([]);
+    const [registrosTempertura, setRegistrosTemperatura] = useState([]);
+    const [registrosHumedad, setRegistrosHumedad] = useState([]);
+    // select
+    const [medida, setMedida] = React.useState('CO2');
+
+    useEffect(() => {
+        const fetchRegistros = async () => {
+            try {
+                const response = await api_registros.listar(token);
+
+                if (response.data.code !== 200) {
+                    enqueueSnackbar(response.data.msg, { variant: "error" });
+                    return;
+                }
+
+                const datos = response.data.datos;
+
+                const co2 = datos.filter((registro: any) => registro.tipo_medicion === "CO2");
+                const temperatura = datos.filter((registro: any) => registro.tipo_medicion === "Temperatura");
+                const humedad = datos.filter((registro: any) => registro.tipo_medicion === "Humedad");
+
+                setRegistros(datos);
+                setRegistrosCO2(co2);
+                setRegistrosTemperatura(temperatura);
+                setRegistrosHumedad(humedad);
+            } catch (error) {
+                enqueueSnackbar("Error al obtener los registros", { variant: "error" });
+                return
+            }
+        };
+        fetchRegistros();
+    }, [token, enqueueSnackbar]);
+
+    const handleChange = (event: any) => {
+        setMedida(event.target.value);
+    };
+
+    // chart color
+    const theme = useTheme();
+    const colors: { [key: string]: string } = {
+        CO2: theme.palette.primary.main,
+        Temperatura: theme.palette.secondary.main,
+        Humedad: theme.palette.success.main,
+    };
+
+    const registroPorTipo: { [key: string]: any[] } = {
+        CO2: registrosCO2,
+        Temperatura: registrosTempertura,
+        Humedad: registrosHumedad,
+    };
+
+    const horas = registroPorTipo[medida].map((registro: any) => registro.hora.slice(0, 5));
+    
+    const valores = registroPorTipo[medida].map((registro: any) => registro.valor_medido);
+    
+    const getUnit = (measurement: string) => {
+        switch (measurement) {
+            case 'CO2':
+                return 'ppm';
+            case 'Temperatura':
+                return '°C';
+            case 'Humedad':
+                return '%';
+            default:
+                return '';
+        }
+    };
+
+    // chart options
+    const optionsColumnChart: any = {
+        chart: {
+            type: 'bar',
+            fontFamily: "'Plus Jakarta Sans', sans-serif;",
+            foreColor: '#adb0bb',
+            toolbar: {
+                show: true,
+                export: {
+                    csv: {
+                        filename: `Mediciones-${medida}`,
+                        columnDelimiter: ';',
+                        headerCategory: 'Hora',
+                        headerValue: 'Value',
+                        dateFormatter(timestamp: number) {
+                            return new Date(timestamp).toDateString();
+                        }
+                    },
+                    svg: {
+                        filename: `Mediciones-${medida}`,
+                    },
+                    png: {
+                        filename: `Mediciones-${medida}`,
+                    },
+                }
+            },
+            height: 370,
+            animations: {
+                easing: 'easeinout',
+                animatedGradually: {
+                    enabled: true,
+                    delay: 150,
+                },
+                dynamicAnimation: {
+                    enabled: true,
+                    speed: 350,
+                }
+            }
+        },
+        colors: [colors[medida]],
+        plotOptions: {
+            bar: {
+                horizontal: false,
+                barHeight: '60%',
+                columnWidth: '40%',
+                borderRadius: [6],
+                borderRadiusApplication: 'end',
+                borderRadiusWhenStacked: 'all',
+            },
+        },
+        stroke: {
+            show: true,
+            width: 5,
+            lineCap: "butt",
+            colors: ["transparent"],
+        },
+        dataLabels: {
+            enabled: false,
+        },
+        legend: {
+            show: false,
+        },
+        grid: {
+            borderColor: 'rgba(0,0,0,0.1)',
+            strokeDashArray: 3,
+            xaxis: {
+                lines: {
+                    show: false,
+                },
+            },
+        },
+        yaxis: {
+            tickAmount: 4,
+            labels: {
+                formatter: (value: number) => `${value} ${getUnit(medida)}`,
+            }
+        },
+        xaxis: {
+            categories: horas,
+            axisBorder: {
+                show: false,
+            },
+        },
+        tooltip: {
+            theme: 'dark',
+            fillSeriesColor: false,
+            y: {
+                formatter: (value: number) => `${value} ${getUnit(medida)}`,
+            }
+        },
+    };
+
+    // chart data
+    const chartData: any = [
+        {
+            name: medida,
+            data: valores,
+        }
+    ];
+
+    return (
+        <DashboardCard title="Valores Medidos" action={
+            <Select
+                labelId="measurement-dd"
+                id="measurement-dd"
+                value={medida}
+                size="small"
+                onChange={handleChange}
+            >
+                <MenuItem value="CO2">CO2</MenuItem>
+                <MenuItem value="Temperatura">Temperatura</MenuItem>
+                <MenuItem value="Humedad">Humedad</MenuItem>
+            </Select>
+        }>
+            <Chart
+                options={optionsColumnChart}
+                series={chartData}
+                type="bar"
+                height={370} 
+                width={"100%"}
+            />
+        </DashboardCard>
+    );
+};
+
+export default GraficaValoresMedidos;
---
+++ frontend-microservicios/src/app/(DashboardLayout)/components/dashboard/NivelesAulaMagna.tsx
@@ -0,0 +1,254 @@
+import {
+  Typography,
+  Box,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Chip,
+} from "@mui/material";
+import DashboardCard from "@/app/(DashboardLayout)//components/shared/DashboardCard";
+import { useEffect, useState } from "react";
+import { api_sensores } from "@/hooks/Api";
+import { getToken } from "@/hooks/SessionUtils";
+
+interface Medicion {
+  fecha: string,
+  hora: string,
+  sensor: string,
+  nivel?: string,
+  nivelBg?: string,
+  valor_medido: number,
+  indicador?: string,
+}
+
+const validarNivel = (sensor : string, valor: number) => {
+  let nivel = "";
+  let nivelBg = "";
+  let indicador = "";
+
+  if (sensor === "CO2") {
+    if (valor <= 400) {
+      nivel = "Bajo";
+      nivelBg = "#228B22";
+      indicador = "Aire muy limpio";
+    } else if (valor > 400 && valor <= 800) {
+      nivel = "Normal";
+      nivelBg = "#32CD32";
+      indicador = "Aire aceptable";
+    } else if (valor > 800 && valor <= 1000) {
+      nivel = "Óptimo";
+      nivelBg = "#7FFF00";
+      indicador = "Aire de buena calidad";
+    } else if (valor > 1000 && valor <= 1500) {
+      nivel = "Alto";
+      nivelBg = " #FFD700";
+      indicador = "Aire ligeramente contaminado";
+    } else if (valor > 1500 && valor <= 2000){
+      nivel = "Muy Alto";
+      nivelBg = "#FFA500";
+      indicador = "Aire contaminado";
+    } else {
+      nivel = "Peligroso";
+      nivelBg = "#FF4500";
+      indicador = "Aire muy contaminado";
+    }
+  } else if (sensor === "Temperatura") {
+    if (valor <= 10) {
+      nivel = "Muy Frío";
+      nivelBg = "#0000FF";
+      indicador = "Temperatura muy baja";
+    } else if (valor > 10 && valor <= 18) {
+      nivel = "Frío";
+      nivelBg = "#1E90FF";
+      indicador = "Temperatura baja";
+    } else if (valor > 18 && valor <= 24) {
+      nivel = "Óptimo";
+      nivelBg = "#7FFF00";
+      indicador = "Temperatura confortable";
+    } else if (valor > 24 && valor <= 30) {
+      nivel = "Cálido";
+      nivelBg = "#FFD700";
+      indicador = "Temperatura alta";
+    } else if (valor > 30 && valor <= 35) {
+      nivel = "Muy Cálido";
+      nivelBg = "#FFA500";
+      indicador = "Temperatura muy alta";
+    } else {
+      nivel = "Peligroso";
+      nivelBg = "#FF4500";
+      indicador = "Temperatura peligrosa";
+    }
+  } else if (sensor === "Humedad" ) {
+    if (valor <= 20) {
+      nivel = "Muy Baja";
+      nivelBg = "#0000FF";
+      indicador = "Humedad muy baja";
+    } else if (valor > 20 && valor <= 40) {
+      nivel = "Baja";
+      nivelBg = "#1E90FF";
+      indicador = "Humedad baja";
+    } else if (valor > 40 && valor <= 60) {
+      nivel = "Óptimo";
+      nivelBg = "#7FFF00";
+      indicador = "Humedad ideal";
+    } else if (valor > 60 && valor <= 80) {
+      nivel = "Alta";
+      nivelBg = "#FFD700";
+      indicador = "Humedad alta";
+    } else if (valor > 80 && valor <= 95) {
+      nivel = "Muy Alta";
+      nivelBg = "#FFA500";
+      indicador = "Humedad muy alta";
+    } else {
+      nivel = "Peligroso";
+      nivelBg = "#FF4500";
+      indicador = "Humedad extremadamente alta";
+    }
+  }
+
+  return { nivel, nivelBg, indicador };
+};
+
+const NivelesAulaMagna = () => {
+  const [mediciones, setMediciones] = useState<Medicion[]>([]);
+
+  useEffect(() => {
+    const fetchRegistros = async () => {
+      const response = await api_sensores.ultimo_registro();
+
+      const datos = response.data.datos;
+
+      const medicionesmap = datos.map((medicion : any) => {
+        const { nivel, nivelBg, indicador } = validarNivel(medicion.tipo_medicion, medicion.registro_climatico[0].valor_medido);
+        return {
+          fecha: medicion.registro_climatico[0].fecha,
+          hora: medicion.registro_climatico[0].hora,
+          sensor: medicion.tipo_medicion,
+          nivel,
+          nivelBg,
+          indicador,
+          valor_medido: medicion.registro_climatico[0].valor_medido,
+        };
+      });
+      
+      setMediciones(medicionesmap);
+    };
+    fetchRegistros();
+  }, []);
+
+  return (
+    <DashboardCard title="Aula Magna">
+      <Box sx={{ overflow: "auto", width: { xs: "280px", sm: "auto" } }}>
+        <Table
+          aria-label="simple table"
+          sx={{
+            whiteSpace: "nowrap",
+            mt: 2,
+          }}
+        >
+          <TableHead>
+            <TableRow>
+              <TableCell>
+                <Typography variant="subtitle2" fontWeight={600}>
+                  Fecha
+                </Typography>
+              </TableCell>
+
+              <TableCell>
+                <Typography variant="subtitle2" fontWeight={600}>
+                  Hora
+                </Typography>
+              </TableCell>
+
+              <TableCell>
+                <Typography variant="subtitle2" fontWeight={600}>
+                  Sensor
+                </Typography>
+              </TableCell>
+
+              <TableCell>
+                <Typography variant="subtitle2" fontWeight={600}>
+                  Nivel
+                </Typography>
+              </TableCell>
+
+              <TableCell>
+                <Typography variant="subtitle2" fontWeight={600}>
+                  Indicador
+                </Typography>
+              </TableCell>
+
+              <TableCell align="right">
+                <Typography variant="subtitle2" fontWeight={600}>
+                  Valor Medido
+                </Typography>
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {mediciones.map((medicion, index) => (
+              <TableRow key={index + 1}>
+                <TableCell>
+                  <Typography
+                    color="textSecondary"
+                    sx={{
+                      fontSize: "15px",
+                      fontWeight: "500",
+                    }}
+                  >
+                    {medicion.fecha}
+                  </Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography
+                    color="textSecondary"
+                    sx={{
+                      fontSize: "15px",
+                      fontWeight: "500",
+                    }}
+                  >
+                    {medicion.hora}
+                  </Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography
+                    color="textSecondary"
+                    fontWeight={400}
+                  >
+                    {medicion.sensor}
+                  </Typography>
+                </TableCell>
+                <TableCell>
+                  <Chip
+                    sx={{
+                      px: "4px",
+                      backgroundColor: medicion.nivelBg,
+                      color: "#fff",
+                    }}
+                    size="small"
+                    label={medicion.nivel}
+                  ></Chip>
+                </TableCell>
+                <TableCell>
+                  <Typography
+                    color="textSecondary"
+                    fontWeight={400}
+                  >
+                    {medicion.indicador}
+                  </Typography>
+                </TableCell>
+                <TableCell align="right">
+                  <Typography variant="h6">{medicion.valor_medido}</Typography>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Box>
+    </DashboardCard>
+  );
+};
+
+export default NivelesAulaMagna;
