22a0ef398ca6402e760e8c9f267807842aa09cb8
Dennys Pucha <dennys.pucha@unl.edu.ec>
2024-07-29 21:29:30 -0500

    updating

--- microservicio-sensores/routes/api.js
+++ microservicio-sensores/routes/api.js
@@ -11,15 +11,14 @@ const auth = require('../middlewares/authMiddleware');
 router.get('/sensores/ultimo_registro', sensorControl.ultimo_registro);
 router.get('/sensores', auth, sensorControl.listar);
 router.get('/sensores/:external', auth, sensorControl.obtener_sensor);
-router.post('/sensores/crear', auth, sensorControl.crear);
+router.post('/sensores/crear', sensorControl.crear);
 router.get('/sensores/registros/:external', auth, sensorControl.obtener_registros_climaticos);
 router.patch('/sensores/actualizar/:external', sensorControl.actualizar);
-router.get('/sensores/ultimo_registro', sensorControl.ultimo_registro);
 router.delete('/sensores/eliminar/:external', auth, sensorControl.eliminar);
 
 //API monitoreo
-router.post('/sensores/iniciar-monitoreo', auth, sensorControl.iniciarMonitoreoTodosSensores.bind(sensorControl));
-router.post('/sensores/detener-monitoreo', auth, (req, res) => sensorControl.detenerMonitoreo(req, res));
+router.post('/sensores/iniciar-monitoreo', sensorControl.iniciarMonitoreoTodosSensores.bind(sensorControl));
+router.post('/sensores/detener-monitoreo', (req, res) => sensorControl.detenerMonitoreo(req, res));
 
 //API registros
 router.get('/registros/listar/hoy', registrosControl.listar_hoy);
